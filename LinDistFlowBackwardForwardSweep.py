fixed jacobian for general no of states

LinDistFlowBackwardForwardSweep.py
@@ -1,8 +1,8 @@
-def LinDistFlowBackwardForwardSweep(P_Load,Q_Load,which):
+def LinDistFlowBackwardForwardSweep(P_Load,Q_Load, which, V0=None):
 
     import numpy as np
     import copy
-    
+
     if which == 37:
         from Network37 import BusNum, bus_arcs, LineData_Z_pu, arcs, Sbase, R_line, X_line
     else:
@@ -27,7 +27,8 @@ def LinDistFlowBackwardForwardSweep(P_Load,Q_Load,which):
     V = {}
     for i in BusNum: #Note that bus 0 here shows the ideal secondary voltages of the transformer
         V[i] = 1 # square magnitude
-    
+    V[0] = 1 if V0 is None else V0
+
     #Initialization of iteration count
     k = 0 # iteration count
     e_max = 1
@@ -55,7 +56,7 @@ def LinDistFlowBackwardForwardSweep(P_Load,Q_Load,which):
         
         #Calculation of error
         e_max = max(abs(V[i] - V_previous[i]) for i in BusNum)
-    
+    Vmag = {key:np.sqrt(val) for key, val in V.items()} # sqrt of mag
     #Report Results
     # V_mag = {}
     # V_ang = {}
@@ -71,4 +72,4 @@ def LinDistFlowBackwardForwardSweep(P_Load,Q_Load,which):
     
     # would want to return current as well in futuret
 
-    return(V, P_line, Q_line, e_max,k)
+    return(V, Vmag, P_line, Q_line, e_max,k)

jacobian_calc.py
@@ -6,19 +6,51 @@ Created on Fri Nov 27 13:44:59 2020
 @author: shub
 """
 import numpy as np
-def create_jacobian(num_states, num_meas):
+def create_jacobian(P_line, P_Load_state, P_Load_meas, path_to_all_nodes,
+                    V, R_line, X_line, num_states, num_meas):
+    
+    num_states, num_meas
+    
+    # initial case jacobian matrix
+    # num_meas = 36*2 + 37*3 # 2 for pij and qij 3 for pj, qj, vj^2
+    # num_states = 2 * 37 + 1 # 2 for pj, qj 1 for v0^2
     jacobian_matrix = np.zeros((num_meas, num_states))
+    
     # call jacobian for pline_plflow
+    grad_array, index_of_lines = grad_pline_with_p(P_line, P_Load_state, path_to_all_nodes)
+    meas_rows = grad_array.shape[0]
+    state_cols = grad_array.shape[1]
+    jacobian_matrix[0:meas_rows, 0:state_cols] = grad_array
+    last_row_inserted = meas_rows
     
     # call jacobian for qline_qflow, should be exactly same as above
+    jacobian_matrix[last_row_inserted:last_row_inserted + meas_rows, state_cols:2*state_cols] = grad_array
+    last_row_inserted = 2*meas_rows # didn't do -1 because then this can be used directly
     
     # call jacobian for ppseudo_p
+    grad_array = grad_pseudo_with_p(P_Load_meas, P_Load_state)
+    meas_rows = grad_array.shape[0]
+    state_cols = grad_array.shape[1]
+    jacobian_matrix[last_row_inserted:last_row_inserted + meas_rows, 0:state_cols] = grad_array
+    last_row_inserted = last_row_inserted + meas_rows
     
     # call jacobian for qpseudo_q, should be same as above
+    jacobian_matrix[last_row_inserted:last_row_inserted + meas_rows, state_cols:2*state_cols] = grad_array
+    last_row_inserted += meas_rows
     
     # call jacobian for vnode^2 with p
+    grad_array = grad_vnode_with_p(V, P_Load_state, path_to_all_nodes, R_line)
+    meas_rows = grad_array.shape[0]
+    state_cols = grad_array.shape[1]
+    jacobian_matrix[last_row_inserted:last_row_inserted + meas_rows, 0:state_cols] = grad_array
     
     # call jacobian for vnode^2 with q
+    grad_array = grad_vnode_with_p(V, P_Load_state, path_to_all_nodes, X_line)
+    jacobian_matrix[last_row_inserted:last_row_inserted + meas_rows, state_cols:2*state_cols] = grad_array
+    
+    # call jacobian with v0^2
+    grad_array = grad_vnode_with_v0(V)
+    jacobian_matrix[last_row_inserted:last_row_inserted + meas_rows, 2*state_cols:] = grad_array
     
     return jacobian_matrix
 
@@ -34,39 +66,44 @@ def grad_pline_with_p(S_line_meas, P_Load_states, path_to_all_nodes):
     grad_array = np.zeros((len(S_line_meas), len(P_Load_states))) # meas*states
 
     index_of_lines = {}
-    for i , (k,v) in enumerate(S_line_meas.items()): # iterate over measurements
+    for i , (k,v) in enumerate(S_line_meas.items()): # iterate over measurements line
         # print(i,k)
         index_of_lines[i] = k
-        # print(i, k, v)
-        for node in P_Load_states.keys(): # iterate over states
-            # print(node)
+        print(i, k, v)
+        for j, node in enumerate(P_Load_states.keys()): # iterate over states node
+            # print(j, node)
             if k in path_to_all_nodes[node]:
                 print(i, k, node)
-                grad_array[i,node] = 1 # it will be a matrix of 0s and 1s
+                # below we use j instead of node becasue not all nodes in the distribution are considered as state
+                # all the zib are known hence not a part of state
+                grad_array[i,j] = 1 # it will be a matrix of 0s and 1s
     return grad_array, index_of_lines
     
 def grad_pseudo_with_p(p_pseudo, p_states):
     grad_array = np.zeros((len(p_pseudo), len(p_states)))
     for node_i in p_pseudo.keys(): # meas node
-        for node_j in p_states.keys(): # state node
+        for j, node_j in enumerate(p_states.keys()): # state node
             if node_i == node_j:
-                grad_array[node_i][node_j] = 1
+                print(node_i, node_j, j)
+                grad_array[node_i][j] = 1
                 break # if match found, break the search
     return grad_array
 
 def grad_vnode_with_p(v_meas, p_states, path_to_all_nodes, R_line):
     grad_array = np.zeros((len(v_meas), len(p_states)))
     for node_i in v_meas.keys(): # meas node
-        for node_j in p_states.keys():
+        for j, node_j in enumerate(p_states.keys()): # state node
             print(node_i, node_j)
             common_lines = path_to_all_nodes[node_i].intersection(path_to_all_nodes[node_j])
-            grad_array[node_i][node_j] = -(sum(R_line[item] for item in common_lines)) * 2
+            grad_array[node_i][j] = -(sum(R_line[item] for item in common_lines)) * 2
     return grad_array
             
 def grad_vnode_with_v0(v_meas):
     grad_array = np.ones((len(v_meas),1))
     return grad_array
 
+'''
+# make jacobian without the function
 # initial case jacobian matrix
 num_meas = 36*2 + 37*3 # 2 for pij and qij 3 for pj, qj, vj^2
 num_states = 2 * 37 + 1 # 2 for pj, qj 1 for v0^2
@@ -107,3 +144,4 @@ jacobian_matrix[last_row_inserted:last_row_inserted + meas_rows, state_cols:2*st
 # call jacobian with v0^2
 grad_array = grad_vnode_with_v0(V)
 jacobian_matrix[last_row_inserted:last_row_inserted + meas_rows, 2*state_cols:] = grad_array
+'''

path_to_nodes.py
@@ -9,24 +9,25 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
-which = 37 # IEEE 37-node or IEEE 906-node
-
-if which == 37:
-    from Network37 import *
+def path_to_nodes(which):
+    if which == 37: # IEEE 37-node or IEEE 906-node
+        from Network37 import arcs
+        
+    # instantiate the graph
+    G = nx.Graph()
+    # add nodes
+    G.add_nodes_from(np.arange(0, which))
+    # add edges
+    G.add_edges_from((arcs))
+    # plot graph
+    plt.figure()
+    nx.draw(G, with_labels=True, font_weight='bold')
     
-# instantiate the graph
-G = nx.Graph()
-# add nodes
-G.add_nodes_from(np.arange(0, which))
-# add edges
-G.add_edges_from((arcs))
-# plot graph
-plt.figure()
-nx.draw(G, with_labels=True, font_weight='bold')
-
-# get the path to nodes from the slack bus i.e. node 0
-path_nodes = [nx.shortest_path(G, 0, i) for i in range(which)]
-# convert to dictinary with vals as set of tuples denoting the path to nodes
-path_to_all_nodes = {key:set(zip(val[:-1], val[1:])) for key,val in enumerate(path_nodes)}
+    # get the path to nodes from the slack bus i.e. node 0
+    path_nodes = [nx.shortest_path(G, 0, i) for i in range(which)]
+    # convert to dictinary with vals as set of tuples denoting the path to nodes
+    path_to_all_nodes = {key:set(zip(val[:-1], val[1:])) for key,val in enumerate(path_nodes)}
+    
+    return path_to_all_nodes
 
 # set(zip(path_nodes[:-1], path_nodes[1:]))

test.py
@@ -1,5 +1,9 @@
 from LinDistFlowBackwardForwardSweep import LinDistFlowBackwardForwardSweep
+from BackwardForwardSweep import BackwardForwardSweep
 import numpy as np
+from jacobian_calc import create_jacobian
+from path_to_nodes import path_to_nodes
+
 which = 37 # IEEE 37-node or IEEE 906-node
 
 if which == 37:
@@ -7,19 +11,17 @@ if which == 37:
 else:
     from Network906 import *    
 
-# [V, P_line, Q_line, e_max, k] =LinDistFlowBackwardForwardSweep(P_Load,Q_Load,which)
+[V, Vmag, P_line, Q_line, e_max, k] = LinDistFlowBackwardForwardSweep(P_Load, Q_Load, which)
 
-[V_mag,V_ang,Voltage,S_line,I_line,I_load,e_max,k] =BackwardForwardSweep(P_Load,Q_Load,which)
-V = V_mag
+# [V_mag,V_ang,Voltage,S_line,I_line,I_load,e_max,k] = BackwardForwardSweep(P_Load, Q_Load,which)
+# Vsq =  {key:val**2 for key, val in V_mag.items()} # square of V_mag
+# V = Vsq
 
 # when running full network
-P_line = {key:val.real for key, val in S_line.items()} # resistance of every line
-Q_line = {key:val.imag for key, val in S_line.items()} # reactancce of every line
-
+# P_line = {key:val.real for key, val in S_line.items()} # resistance of every line
+# Q_line = {key:val.imag for key, val in S_line.items()} # reactancce of every line
 
 # below is state estimation
-
-# get jacobain matrix
 
 # ground truth
 gt_P_load = list(P_Load.values())
@@ -31,42 +33,72 @@ x = np.insert(x, len(x), gt_V) # ground truth for states
 # states = ['P_Load', 'Q_Load', 'V0']
 # estimate_states(states)
 
-
-
 # measurement vector
 meas_P_line = list(P_line.values())
 meas_Q_line = list(Q_line.values())
 meas_P_load = list(P_Load.values())
 meas_Q_load = list(Q_Load.values())
 meas_V = list(V.values())
-z = np.asarray(meas_P_line + meas_Q_line + meas_P_load + meas_Q_load + meas_V) # ground truth for meas
-weight_array = np.ones((len(z)))*0.04
+z_true = np.asarray(meas_P_line + meas_Q_line + meas_P_load + meas_Q_load + meas_V) # ground truth for meas
+
+var = 0.04
+weight_array = np.ones((len(z_true)))*var
 # weight_array = np.insert(weight_array, len(weight_array), 0.00001)
 W = np.diag(weight_array) # Weight mat
 W = np.linalg.inv(W)
 
-# initialze state vars
-# remove p0 = 0 and the rest have values equally divided from p_ij
-p_distributed = P_line[(0,1)]/(len(P_Load) - 1)
-p_states = np.zeros((len(P_Load)-1)) + p_distributed
-p_states = np.insert(p_states, 0, 0) # insert 0 at loc 0
+# add noise to measurement set
+mu, sigma = 0, 0.01 # mean and standatd deviation
+noise = np.random.normal(mu, sigma, len(z_true))
+# noise = 0
+z = z_true + noise # noisy data
 
-q_distributed = Q_line[(0,1)]/(len(Q_Load) - 1)
-q_states = np.zeros((len(Q_Load)-1)) + q_distributed
-q_states = np.insert(q_states, 0, 0) # insert 0 at loc 0
+# initialze state vars
+
+# consider the state vars only for non ZIBs
+P_Load_state = {}
+zib_index, non_zib_index = [], [] # index of zibs and non zibs
+for k,v in P_Load.items():
+    if v != 0:
+        P_Load_state[k] = v
+        non_zib_index.append(k)
+    else:
+        zib_index.append(k)
+        
+# remove p0 = 0 and the rest have values equally divided from p_ij
+p_distributed = P_line[(0,1)]/(len(P_Load_state))
+p_states = np.zeros((len(P_Load_state))) + p_distributed
+# p_states = np.insert(p_states, 0, 0) # insert 0 at loc 0
+
+q_distributed = Q_line[(0,1)]/(len(P_Load_state))
+q_states = np.zeros((len(P_Load_state))) + q_distributed
+# q_states = np.insert(q_states, 0, 0) # insert 0 at loc 0
 
 v0 = 1 # slack bus
 
 x_est = np.concatenate((p_states, q_states))
 x_est = np.insert(x_est, len(x_est), v0) # initialized state vars
 
-# some preprocessing for
+# get paths from slack bus to all nodes
+path_to_all_nodes = path_to_nodes(which)
+
+# get jacobain matrix
+# jacobian_matrix = create_jacobian(P_line, P_Load_state, P_Load_meas, path_to_all_nodes,
+#                                   V, R_line, X_line, len(x_est), len(z))
+
+
+# we arent using the values of P_line, P_Load_state or P_Load in jacobian_calc
+# only their keys
+jacobian_matrix = create_jacobian(P_line, P_Load_state, P_Load, path_to_all_nodes,
+                                  V, R_line, X_line, len(x_est), len(z))
+
+# some preprocessing for time saving during iterative newton method
 G = np.matmul(np.matmul(jacobian_matrix.T, W), jacobian_matrix)
 Ginv = np.linalg.inv(G)
 
 count = 0
-delta_mat = np.zeros((len(x), 100))
-tol = 10e-12
+delta_mat = np.zeros((len(x_est), 100000))
+tol = 10e-16
 results = x_est
 emax = 100 # chosen higher than the tol
 
@@ -94,10 +126,36 @@ while emax > tol:
     results = np.vstack((results, x_est))
     count+=1
     print(count-1)
-    
+
+# get the full vector for xest
+full_x_est = np.zeros((len(x)))
+full_x_est[non_zib_index] = x_est[0:len(non_zib_index)] # insert p vals
+full_x_est[len(P_Load)+np.asarray(non_zib_index)] = x_est[len(non_zib_index):2*len(non_zib_index)] # insert q vals
+full_x_est[-1] = x_est[-1]
+
 # calculate error between state vectors
-error = x - x_est
+error = x - full_x_est
 max_error = np.max(abs(error))
+
 # sum of residuals
 sum_residuals = np.sum(abs(residuals))
 results = results.T
+
+# Regenerated measurements
+# format it for lindistflow func
+keys = list(range(len(P_Load)))
+array = full_x_est[0:len(P_Load)]
+P_Load_est = dict(zip(keys, array))
+Q_Load_est = dict(zip(keys, full_x_est[len(P_Load):len(P_Load)*2]))
+[V_con, _ ,P_line_con, Q_line_con, e_max_con, k_con] = LinDistFlowBackwardForwardSweep(
+    P_Load_est, Q_Load_est, which, full_x_est[-1])
+
+# # error calc between 
+# meas_P_line = list(P_line_con.values())
+# meas_Q_line = list(Q_line_con.values())
+# meas_P_load = list(P_Load.values())
+# meas_Q_load = list(Q_Load.values())
+# meas_V = list(V_con.values())
+# z_true = np.asarray(meas_P_line + meas_Q_line + meas_P_load + meas_Q_load + meas_V) # ground truth for meas
+
+# fix/ come up with error calc