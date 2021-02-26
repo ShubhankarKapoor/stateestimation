d277cb1523c0bddda61f9cd833bd3be392cca6ed
Shub <shubhankarkapoor94@gmail.com>
2021-02-26 12:09:38 +1100

    added pflow-qflow loss in GN

--- test.py
+++ test.py
@@ -98,7 +98,7 @@ x_true = np.insert(x_true, len(x_true), gt_V) # ground truth for states
 ##############################################################################
 
 # get subset of lineflow measurement set
-num_plow_meas = 1
+num_plow_meas = 2
 num_voltage_meas = 1
 # chose lineflows
 meas_P_line, meas_Q_line = subset_of_measurements(
