ab793b04a1eed323d7b6f3758d5ea8a5cfde463e
shub <shubhankarkapoor94@gmail.com>
2022-03-31 15:33:41 +1100

    modifying new plots code

--- new_plots.py
+++ new_plots.py
@@ -9,28 +9,65 @@ Created on Mon Mar 28 13:25:13 2022
 # chose 2 cases for comparison
 # use the following for plotting
 # maybe pick paper and pen to decide what 2 cases you want to do
-import networkx as nx  
-s = 5
-G = nx.grid_graph(dim=[s,s])
-nodes = list(G.nodes)
-edges = list(G.edges)
-p = []
-for i in range(0, s):
-    for j in range(0, s):
-        p.append([i,j])
-for i in range(0, len(nodes)):
-    G.nodes[nodes[i]]['pos'] = p[i]
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
 
-pos = {}
-for i in range(0, len(nodes)):
-        pos[nodes[i]] = p[i]
+SMALL_SIZE = 8
+MEDIUM_SIZE = 15
+BIGGER_SIZE = 22
 
-from random import randint
-val = []
-for i in range(0, len(G.nodes())):
-    val.append(randint(0,4))
+plt.rc('font', size=MEDIUM_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=BIGGER_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=MEDIUM_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)
 
-nx.draw(G, pos, node_color=val)
+# s = 5
+# G = nx.grid_graph(dim=[s,s])
+# nodes = list(G.nodes)
+# edges = list(G.edges)
+# p = []
+# for i in range(0, s):
+#     for j in range(0, s):
+#         p.append([i,j])
+# for i in range(0, len(nodes)):
+#     G.nodes[nodes[i]]['pos'] = p[i]
+
+# pos = {}
+# for i in range(0, len(nodes)):
+#         pos[nodes[i]] = p[i]
+
+# from random import randint
+# val = []
+# for i in range(0, len(G.nodes())):
+#     val.append(randint(0,4))
+
+# nx.draw(G, pos, node_color=val)
+
+if which == 37: # IEEE 37-node or IEEE 906-node
+    from Network37 import arcs
+if which == 906 or which == 907: # IEEE 37-node or IEEE 906-node
+    from Network906 import arcs
+    which = 906 + 1 # becasue there are 907 buses, see nw 906 script
+
+if vmin == None and np.all(val)!= None:
+    vmin = np.min(val)
+if vmin == None and np.all(val)!= None:
+    vmax = np.max(val)
+# instantiate the graph
+G = nx.Graph()
+# add nodes
+G.add_nodes_from(np.arange(0, which))
+# add edges
+if which == 37 or which == 906 or which == 907:
+    G.add_edges_from((arcs))
+else: # use node_a and node_b for edge addition
+    e = zip(node_a, node_b)
+    G.add_edges_from(e)
 
 # use the following for distribution of errors
 # plot v perc hist for all methods
@@ -43,3 +80,27 @@ ax.bar(range(len(hist)),hist,width=1,align='center',tick_label=
         ['{} - {}'.format(bins[i],bins[i+1]) for i,j in enumerate(hist)])
 
 # plot p abs hist for all methods
+
+# plotting heatmap on nodes
+# just do it using proposed methodology for diff cases of meas available
+# do it for voltage percentage and abs power
+val = abs_p_la
+# val = perc_v_la
+plt.figure()
+with open('saved_pos.pkl', 'rb') as f:
+    pos = pickle.load(f) # load the position of nodes for plotting
+if np.all(val) == None:
+    # nx.draw(G, pos=pos, with_labels=True, font_weight='bold')
+    nx.draw(G, pos=pos, with_labels=True, font_weight='bold')
+    plt.title('Test Feeder {}'.format(which))
+else:
+    # define vmin,vmax
+    # plt.title('Voltage Percentage Error at each node in Test Feeder {}'.format(which))
+    plt.title('Active Power Absolute Error (kW) at Each node in Test Feeder {}'.format(which))
+    cmap = plt.cm.Blues
+    # nx.draw(G, pos=pos, with_labels=True, font_weight='bold', node_color=val)
+    nx.draw(G, pos=pos, vmin = vmin, vmax = vmax, with_labels=True, 
+            font_weight='bold', node_color=val, cmap=cmap)
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin = vmin, vmax=vmax))
+    sm._A = []
+    plt.colorbar(sm)
